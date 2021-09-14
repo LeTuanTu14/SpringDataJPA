d847d4ca50aa0bddf74ce98d453bd800b1d158c2
TUAN TU <letuantu1432k@gmail.com>
2021-09-14 11:42:24 +0700

    commit jpql

--- src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -2,6 +2,28 @@ package com.example.springdatajpa.repository;
 
 import com.example.springdatajpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
+    @Query("select s from Student s")
+    List<Student> getAll();
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into student(name, email) values(?1,?2)", nativeQuery = true)
+    void addStudent(String name, String email);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update student t set t.name = ?2, t.email =?3 where t.id = ?1", nativeQuery = true)
+    void updateStudent(Integer id, String name, String email);
+
+    @Transactional
+    @Modifying
+    @Query("delete from Student t where t.id = ?1")
+    void deleteStudentById(Integer id);
 }
---
+++ src/test/java/com/example/springdatajpa/StudentResposityTest.java
@@ -0,0 +1,33 @@
+package com.example.springdatajpa;
+
+import com.example.springdatajpa.repository.StudentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StudentResposityTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void getAll() {
+        System.out.println("getAll: " + studentRepository.getAll());
+    }
+
+    @Test
+    public void add() {
+        studentRepository.addStudent("Tuan Tu", "abc@gmail.com");
+    }
+
+    @Test
+    public void update() {
+        studentRepository.updateStudent(1,"Tuan Tu 14", "tu@gmail.com");
+    }
+
+    @Test
+    public void delete() {
+        studentRepository.deleteStudentById(1);
+    }
+}
