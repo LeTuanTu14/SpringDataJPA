bd51d50cef31ffcea7396c914c45243287813b07
TUAN TU <letuantu1432k@gmail.com>
2021-09-14 12:13:41 +0700

    complete repo

--- src/main/java/com/example/springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/example/springdatajpa/repository/StudentRepository.java
@@ -9,21 +9,5 @@ import javax.transaction.Transactional;
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Long>{
-    @Query(value = "select * from student", nativeQuery = true)
-    List<Student> getAll();
 
-    @Transactional
-    @Modifying
-    @Query(value = "insert into student(name, email) values(?1,?2)", nativeQuery = true)
-    void addStudent(String name, String email);
-
-    @Transactional
-    @Modifying
-    @Query(value = "update student t set t.name = ?2, t.email =?3 where t.id = ?1", nativeQuery = true)
-    void updateStudent(Integer id, String name, String email);
-
-    @Transactional
-    @Modifying
-    @Query(value ="delete from student t where t.id = ?1", nativeQuery = true)
-    void deleteStudentById(Integer id);
 }
